14bf0e32ebc65c8db9559daaaa0e333038be87e3
Krishna Raj B <bkr.2007@yahoo.com>
2026-09-05 19:34:11 +0530

    test(pcb): expose repro error in test name

--- tests/binary-pcb-opaque-connection-stream-repro.test.ts
+++ tests/binary-pcb-opaque-connection-stream-repro.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "bun:test"
 import { parseAltiumBinaryPcbDoc } from "../lib"
 import { readReferenceBytes } from "./svg/read-reference"
 
-test("captures the opaque Connections6 parser error", async () => {
+test('reproduces error: Connection property record at offset 0 does not begin with "|"', async () => {
   const source = await readReferenceBytes("dsp5509-ciii.PcbDoc")
 
   let parserError: unknown
